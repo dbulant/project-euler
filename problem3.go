d99eb19eb894942abc218d2f46d49f831bc9cc8b
David Bulant <david.bulant@moderntv.eu>
2017-11-19 20:26:51 +0100

    Testing ssh

--- problem3.go
+++ problem3.go
@@ -76,3 +76,7 @@ func deleteMultipliersFromList(mul uint64, numbers []uint64) int {
 	//fmt.Printf("len of numbers is: %d \n", len(numbers))
 	//fmt.Printf("cap of numbers is: %d \n", cap(numbers))
 }
+
+func testingSSH() {
+
+}
