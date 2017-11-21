f65c9329ab3e62878e49455458659bd20de66468
David Bulant <david.bulant@moderntv.eu>
2017-11-21 14:33:37 +0100

    Basics of Problem 11.

---
+++ problem11_test.go
@@ -0,0 +1,18 @@
+// problem11_test
+package projecteuler
+
+import (
+	"testing"
+)
+
+func TestGridToMatrix(t *testing.T) {
+	m := gridToMatrix(GRID)
+	maxValueFromLeftToRight(m, 4)
+	max := maxDiagonally(m, 4)
+	t.Errorf("max is %d \n", max)
+
+	/*
+		m := gridToMatrix(GRID)
+		t.Errorf("matrix is: %+v \n", m)
+	*/
+}
