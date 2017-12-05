716cf50c0a3a3dc1d82636cf7b095e7d7762da8a
David Bulant <david.bulant@moderntv.eu>
2017-12-05 14:36:59 +0100

    Getting exponent,decimal and fractional part from float

--- problem20.go
+++ problem20.go
@@ -1,20 +1,23 @@
 // problem20
 package projecteuler
 
-func Problem20() {
-
+func Problem20() uint64 {
+	return GeneralProblem20(100)
 }
 
 func GeneralProblem20(n uint64) uint64 {
-	product := factorial(n)
-	digits := getDigitsFromNumber(product)
-	sum := sumOfSlice(digits)
-	return sum
+	/*
+		product := factorial(n)
+		digits := getDigitsFromNumber(product)
+		sum := sumOfSlice(digits)
+		return sum
+	*/
+	return 1
 }
 
-func factorial(n uint64) uint64 {
-	var product uint64 = 1
-	for i := uint64(1); i <= n; i++ {
+func factorial(n float64) float64 {
+	var product float64 = 1
+	for i := float64(1); i <= n; i++ {
 		product *= i
 	}
 	return product
--- problem20_test.go
+++ problem20_test.go
@@ -4,15 +4,37 @@ package projecteuler
 import "testing"
 
 func TestFactorial(t *testing.T) {
-	var expected uint64 = 3628800
+	var expected float64 = 3628800
 	f := factorial(10)
 	if f != expected {
-		t.Errorf("expected %d, got %d instead \n", expected, f)
+		t.Errorf("expected %e, got %e instead \n", expected, f)
 	}
+
+	//TODO no hardcoded values this way
+	expected = 9.33262154439441e+157
+	f = factorial(100)
+	//TODO obviously do not use this operator
+	if f != expected {
+		t.Errorf("expected %g \n", expected)
+		t.Errorf("got %g instead \n", f)
+		t.Errorf("difference is %g \n", expected-f)
+	}
+
+	data, err := getDigitsFromFloatNumber(f)
+	if err != nil {
+		t.Errorf("Errot during string-float operstions is %v \n", err)
+	}
+	t.Errorf("data struct is %+v \n", data)
 }
 
 func TestGeneralProblem20(t *testing.T) {
+	var expected uint64 = 27
 	r := GeneralProblem20(10)
+	if r != expected {
+		t.Errorf("expected %d, got %d instead \n", expected, r)
+	}
+
+	r = Problem20()
 	if r != 27 {
 		t.Errorf("expected %d, got %d instead \n", 27, r)
 	}
