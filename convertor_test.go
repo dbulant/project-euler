4ed3f35eb0fb72d15eaf74a8331ac014eae92128
David Bulant <david.bulant@moderntv.eu>
2017-12-08 19:32:07 +0100

    Converting without log messages.

--- convertor_test.go
+++ convertor_test.go
@@ -7,13 +7,15 @@ import (
 
 func TestConvertor(t *testing.T) {
 	digs := make([]uint64, 0, 10)
-	//digs := evenUint64ToBinary(8)
-	//t.Errorf("digs are %+v \n", digs)
+	digs = evenUint64ToBinary(14)
+	t.Errorf("digs are %+v \n", digs)
 
 	digs = evenUint64ToBinary(32)
 	t.Errorf("digs are %+v \n", digs)
-	/*
-		digs = uint64ToBinary(5)
-		t.Errorf("digs are %+v \n", digs)
-	*/
+
+	digs = oddUint64ToBinary(5)
+	t.Errorf("digs are %+v \n", digs)
+
+	digs = oddUint64ToBinary(17)
+	t.Errorf("digs are %+v \n", digs)
 }
